e2b86d92d63aaec1014df3ff504490d7bf38fb6c
Ashish0105 <aranjan008@gmail.com>
2023-01-31 21:27:26 +0530

    credential

--- demo.sh
+++ demo.sh
@@ -9,3 +9,7 @@ export DATABASE_NAME='UserDatabase'
 export USER_COLLECTION_NAME='User'
 
 export EMBEDDING_COLLECTION_NAME='Embedding'
+
+#az ad sp create-for-rbac --name "faceapp123_login_role" --role contributor --scopes /subscriptions/d69f66e6-dfeb-462f-8dfe-1b63e57a548b/resourceGroups/faceapp123 --sdk-auth
+
+##
