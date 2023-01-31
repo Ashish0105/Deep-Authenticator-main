6c8cf3e8dddb9b04707c2f999533ce08a3ec6a31
Ashish0105 <aranjan008@gmail.com>
2023-01-31 20:34:36 +0530

    demo

---
+++ demo.sh
@@ -0,0 +1,11 @@
+export SECRET_KEY=KlgH6AzYDeZeGwD288to79I3vTHT8wp7
+
+export ALGORITHM=HS256
+
+export MONGODB_URL_KEY="mongodb://localhost:27017"
+
+export DATABASE_NAME='UserDatabase'
+
+export USER_COLLECTION_NAME='User'
+
+export EMBEDDING_COLLECTION_NAME='Embedding'
